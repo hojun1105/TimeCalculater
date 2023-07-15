6aac0dd9cda564b872f8d4591a25c5f11bd63b18
hojunJeong <hojun1105@gmail.com>
2023-07-15 12:23:08 +0900

    DayModel onproperty changed처리

--- TimeCalculater/DayModel.cs
+++ TimeCalculater/DayModel.cs
@@ -7,12 +7,46 @@ using System.Threading.Tasks;
 
 namespace TimeCalculater
 {
-    public class DayModel
+    public class DayModel : INotifyPropertyChanged
     {
-        public string StartTime {get;set;}
-        public string EndTime {get; set;}
+        public string _StartTime;
+        public string StartTime 
+        {
+            get => _StartTime;
+            set 
+            { 
+                if (value != _StartTime)
+                {
+                    _StartTime = value;
+                    OnPropertyChanged(nameof(StartTime));
+                } 
+            }
+        }
+
+        public string _EndTime;
+        public string EndTime
+        {
+            get => _EndTime;
+            set
+            {
+                if (value != _EndTime)
+                {
+                    _EndTime = value;
+                    OnPropertyChanged(nameof(EndTime));
+                }
+            }
+        }
         public DateTime RoundedStartTime { get; set;}
         public DateTime RoundedEndTime { get; set;}
         public TimeSpan WorkDuration { get; set; }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
