67af476fda00aa5fe28e05204cbbce23f481582b
hojun1105 <hjjeong9794**>
2023-08-18 09:46:31 +0900

    DriverManager로 chromeDriver버전 맞춰줌

--- TimeCalculater/Crawler.cs
+++ TimeCalculater/Crawler.cs
@@ -1,115 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
-using static System.Net.Mime.MediaTypeNames;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
-namespace TimeCalculator
+namespace TimeCalculator;
+
+public class Crawler
 {
-    public class Crawler
+    private readonly string _id;
+    private readonly string _password;
+
+    private List<string> xPathList;
+
+    public Crawler(string id, string password)
     {
-        private string _id;
-        private string _password;
-        public Crawler(string id, string password)
-        {
-            _id = id;
-            _password = password;
-            SetXPath();
-        }
-
-        private List<string> xPathList;
-
-        private void SetXPath() 
-        {
-            xPathList = new List<string>()
-            {
-                "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[1]/div/div[2]/div[2]/span",
-                "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[1]/div/div[2]/div[4]/span",
-                "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[2]/div/div[2]/div[2]/span",
-                "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[2]/div/div[2]/div[4]/span",
-                "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[3]/div/div[2]/div[2]/span",
-                "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[3]/div/div[2]/div[4]/span",
-                "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[4]/div/div[2]/div[2]/span",
-                "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[4]/div/div[2]/div[4]/span",
-                "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[5]/div/div[2]/div[2]/span",
-                "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[5]/div/div[2]/div[4]/span"
-            };
+        _id = id;
+        _password = password;
+        SetXPath();
     }
 
-        #region Method
-
-        public List<string> Crawl()
+    private void SetXPath()
+    {
+        xPathList = new List<string>
         {
-            Login(out ChromeDriver driver);
-            return CrawlDate(driver);
+            "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[1]/div/div[2]/div[2]/span",
+            "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[1]/div/div[2]/div[4]/span",
+            "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[2]/div/div[2]/div[2]/span",
+            "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[2]/div/div[2]/div[4]/span",
+            "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[3]/div/div[2]/div[2]/span",
+            "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[3]/div/div[2]/div[4]/span",
+            "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[4]/div/div[2]/div[2]/span",
+            "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[4]/div/div[2]/div[4]/span",
+            "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[5]/div/div[2]/div[2]/span",
+            "//*[@id=\'contents\']/div/section[3]/div/section/div[2]/table/tbody/tr/td[5]/div/div[2]/div[4]/span"
+        };
+    }
+
+    #region Method
+
+    public List<string> Crawl()
+    {
+        Login(out var driver);
+        return CrawlDate(driver);
+    }
+
+    public void Login(out ChromeDriver driver)
+    {
+        new DriverManager().SetUpDriver(new ChromeConfig());
+        var driverService = ChromeDriverService.CreateDefaultService();
+        driverService.HideCommandPromptWindow = true;
+
+        var options = new ChromeOptions();
+        options.AddArgument("headless");
+        options.AddArgument("ignore-certificate-errors");
+        driver = new ChromeDriver(driverService);
+        driver.Navigate().GoToUrl("https://login.office.hiworks.com/smartdoctor.onhiworks.com");
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        var loginInput =
+            wait.Until(ExpectedConditions.ElementExists(
+                By.XPath("//*[@id=\'root\']/div/main/div/div[1]/form/fieldset/div[2]/div/input")));
+        loginInput.SendKeys(_id);
+
+        var button =
+            wait.Until(ExpectedConditions.ElementExists(
+                By.XPath("//*[@id=\'root\']/div/main/div/div[1]/form/fieldset/button")));
+        button.Click();
+
+        var passWordInput =
+            wait.Until(ExpectedConditions.ElementExists(
+                By.CssSelector("input[class*='mantine-TextInput-input'][type='password']")));
+        passWordInput.SendKeys(_password);
+        button = wait.Until(
+            ExpectedConditions.ElementExists(By.XPath("//*[@id=\'root\']/div/main/div/div[1]/form/fieldset/button")));
+        button.Click();
+
+        try
+        {
+            wait.Until(
+                ExpectedConditions.ElementIsVisible(By.XPath("//*[@id=\'contents\']/div[2]/div[1]/div[1]/div/p")));
         }
-
-        public void Login(out ChromeDriver driver)
+        catch (TimeoutException)
         {
-            var driverService = ChromeDriverService.CreateDefaultService();
-            driverService.HideCommandPromptWindow = true;
+            Console.WriteLine("수동 로그인이 제 시간에 완료되지 않았습니다.");
+        }
+    }
 
-            ChromeOptions options = new ChromeOptions();
-            options.AddArgument("headless");
-            options.AddArgument("ignore-certificate-errors");
-            driver = new ChromeDriver(driverService, options);
-            driver.Navigate().GoToUrl("https://login.office.hiworks.com/smartdoctor.onhiworks.com");
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
+    public List<string> CrawlDate(ChromeDriver driver)
+    {
+        var waitForTimeData = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+        var timeDataList = new List<string>();
 
-            var loginInput = wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\'root\']/div/main/div/div[1]/form/fieldset/div[2]/div/input")));
-            loginInput.SendKeys(_id);
-            
-            var button = wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\'root\']/div/main/div/div[1]/form/fieldset/button")));
-            button.Click();
-
-            var passWordInput = wait.Until(ExpectedConditions.ElementExists(By.CssSelector("input[class*='mantine-TextInput-input'][type='password']")));
-            passWordInput.SendKeys(_password);
-            button = wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\'root\']/div/main/div/div[1]/form/fieldset/button")));
-            button.Click();
-
+        driver.Navigate().GoToUrl("https://hr-work.office.hiworks.com/personal/index");
+        foreach (var path in xPathList)
+        {
+            var aPath = By.XPath(path);
             try
             {
-                wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id=\'contents\']/div[2]/div[1]/div[1]/div/p")));
+                var element = waitForTimeData.Until(ExpectedConditions.ElementExists(aPath));
+                timeDataList.Add(element.Text);
             }
-            catch (TimeoutException)
+            catch (Exception)
             {
-                Console.WriteLine("수동 로그인이 제 시간에 완료되지 않았습니다.");
             }
         }
 
-        public List<string> CrawlDate(ChromeDriver driver)
-        {
-            WebDriverWait waitForTimeData = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
-            var timeDataList = new List<string>();
-            
-            driver.Navigate().GoToUrl("https://hr-work.office.hiworks.com/personal/index");       
-            foreach (string path in xPathList)
-            {
-                
-                By aPath = By.XPath(path);
-                try
-                {
-                    var element = waitForTimeData.Until(ExpectedConditions.ElementExists(aPath));
-                    timeDataList.Add(element.Text);
-                }
-                catch (Exception)
-                {
-                    continue;
-                }
-            }
-            return timeDataList;
-        }
-
-        #endregion
+        return timeDataList;
     }
+
+    #endregion
 }
